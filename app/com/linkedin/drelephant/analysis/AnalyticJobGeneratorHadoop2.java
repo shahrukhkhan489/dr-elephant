9ca733c4a5fc849340a639a54fd6048065a73381
Shahrukh Khan <shahrukhkhan489@gmail.com>
2017-10-28 02:38:00 +0530

    Updating AnalyticJobGeneratorHadoop2.java to resovle the MR Job listing Conflict

--- app/com/linkedin/drelephant/analysis/AnalyticJobGeneratorHadoop2.java
+++ app/com/linkedin/drelephant/analysis/AnalyticJobGeneratorHadoop2.java
@@ -198,7 +198,7 @@ public class AnalyticJobGeneratorHadoop2 implements AnalyticJobGenerator {
    */
   private JsonNode readJsonNode(URL url)
       throws IOException, AuthenticationException {
-    HttpURLConnection conn = _authenticatedURL.openConnection(url, _token);
+//    HttpURLConnection conn = _authenticatedURL.openConnection(url, _token);
     return _objectMapper.readTree(conn.getInputStream());
   }
 
